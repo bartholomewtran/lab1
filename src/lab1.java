09a7fe34552ce94b207512b4ac692adb0103a68b
Bartholomew Tran <tranbartholomew@gmail.com>
2025-10-02 18:17:31 -0700

    Update lab1.java
    added math for future reference

--- src/lab1.java
+++ src/lab1.java
@@ -19,6 +19,17 @@ public class lab1 {
         System.out.print("Loan Amount: " + loanAmt);
         System.out.print("Number of Years: " + loanYear);
         System.out.print("Annual Interest Rate: " + loanRate);
+
+        // Create preset formulas
+
+        annualInterestRate = annualInterestRate / 100;
+        i - monthlyInterestRate = annualInterestRate / 12;
+        n = loanYear * 12;
+        monthlyPayment = (loanAmount * i * Math.pow(i + 1, n)) /;
+        (Math.pow(i + 1, n) - 1);
+        totalPayment = monthlyPayment * 12;
+        // Calculate monthly payment
+        
     }
 }
 
